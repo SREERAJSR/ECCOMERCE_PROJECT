0092e0bceffb2a8456dcb712c3ee132c4dfcb2f0
SREERAJ <admin@SREERAJ-S-R-MacBook-Air.local>
2023-08-02 16:01:28 +0530

    user mobile navbar fixed bug

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -77,36 +77,36 @@
 
     /*==================================================================
     [ Menu mobile ]*/
-    $('.btn-show-menu-mobile').on('click', function(){
-        $(this).toggleClass('is-active');
-        $('.menu-mobile').slideToggle();
-    });
+    // $('.btn-show-menu-mobile').on('click', function(){
+    //     $(this).toggleClass('is-active');
+    //     $('.menu-mobile').slideToggle();
+    // });
 
-    var arrowMainMenu = $('.arrow-main-menu-m');
+    // var arrowMainMenu = $('.arrow-main-menu-m');
 
-    for(var i=0; i<arrowMainMenu.length; i++){
-        $(arrowMainMenu[i]).on('click', function(){
-            $(this).parent().find('.sub-menu-m').slideToggle();
-            $(this).toggleClass('turn-arrow-main-menu-m');
-        })
-    }
+    // for(var i=0; i<arrowMainMenu.length; i++){
+    //     $(arrowMainMenu[i]).on('click', function(){
+    //         $(this).parent().find('.sub-menu-m').slideToggle();
+    //         $(this).toggleClass('turn-arrow-main-menu-m');
+    //     })
+    // }
 
-    $(window).resize(function(){
-        if($(window).width() >= 992){
-            if($('.menu-mobile').css('display') == 'block') {
-                $('.menu-mobile').css('display','none');
-                $('.btn-show-menu-mobile').toggleClass('is-active');
-            }
+    // $(window).resize(function(){
+    //     if($(window).width() >= 992){
+    //         if($('.menu-mobile').css('display') == 'block') {
+    //             $('.menu-mobile').css('display','none');
+    //             $('.btn-show-menu-mobile').toggleClass('is-active');
+    //         }
 
-            $('.sub-menu-m').each(function(){
-                if($(this).css('display') == 'block') { console.log('hello');
-                    $(this).css('display','none');
-                    $(arrowMainMenu).removeClass('turn-arrow-main-menu-m');
-                }
-            });
+    //         $('.sub-menu-m').each(function(){
+    //             if($(this).css('display') == 'block') { console.log('hello');
+    //                 $(this).css('display','none');
+    //                 $(arrowMainMenu).removeClass('turn-arrow-main-menu-m');
+    //             }
+    //         });
                 
-        }
-    });
+    //     }
+    // });
 
 
     /*==================================================================
